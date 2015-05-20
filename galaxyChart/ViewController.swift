f7f9910ad6178e8809699ec75c51da80ba99ee9a
Javier <javier.loucim@gmail.com>
2015-05-20 10:15:20 -0300

    Initial Commit

---
+++ galaxyChart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  galaxyChart
+//
+//  Created by Javi on 20/5/15.
+//  Copyright (c) 2015 Javier Loucim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
